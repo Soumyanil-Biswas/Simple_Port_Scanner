98961b4c316dacf7411543b91477751e19b279c0
Soumyanil Biswas <61424547+Soumyanil-Biswas@users.noreply.github.com>
2021-01-24 00:55:16 +0000

    Updated

--- simple_port_scanner.py
+++ simple_port_scanner.py
@@ -1,45 +1,43 @@
-#!/bin/python3
+#!/usr/bin/python3
 
-from pyfiglet import Figlet
+import nmap
 import time
-import socket
 
-class bcolors:
-    PURPLE = '\033[95m'
-    BLUE = '\033[94m'
-    CYAN = '\033[96m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+RED = '\033[91m'
+PURPLE = '\033[95m'
+ENDC = '\033[0m'
 
-def connectScan(ip,port):
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-	socket.setdefaulttimeout(1)
-	conn=s.connect_ex((ip,port))
-	if(conn == 0) :
-		print(bcolors.GREEN+ f"Port {port} Open On {ip}"+bcolors.ENDC)
-	s.close()
+s_port = int(input("Enter Starting port: "))
+e_port = int(input("Enter Ending port: "))
 
-if __name__ == "__main__":
-	f = Figlet(font='slant')
-	print(f.renderText('Simple Port Scanner'))
-	
-	ip=input(bcolors.BOLD + bcolors.PURPLE + "[+] Enter IP : " + bcolors.ENDC)
-	start=int(input(bcolors.BOLD + bcolors.CYAN + "[+] Enter Start Port : " + bcolors.ENDC))
-	stop=int(input(bcolors.BOLD + bcolors.RED + "[+] Enter Start Port : " + bcolors.ENDC))
+trgt_ip = input("Enter target: ")
 
-	startTime=time.time()
-	print(bcolors.CYAN+f"\n[+] Starting Connect Scan on {ip} from Ports : {start} to {stop}\n")
+# input validation -> IP excludes only space from string
+trgt_ip = trgt_ip.strip()
 
-	while (start<=stop):
-		connectScan(ip,start)
-		start=start+1
-	
-	totalTime = time.time() - startTime
-	totalTime='%.3f'%totalTime
-	print(bcolors.CYAN+f"\n[+] Scan Completed\n[+] Time Taken : {totalTime}s\n")
+scanner = nmap.PortScanner()
 
-	
+#For loop for iteration
+
+print("-"*25)
+
+startTime = time.time()
+
+for i in range(s_port, e_port+1):
+
+    res = scanner.scan(trgt_ip, str(i))
+
+    res = res['scan'][trgt_ip]['tcp'][i]['state']
+
+    # to show the results, we are acessing only 2 values, port
+    # and state of port
+
+    if(res == 'closed'):
+        print('| Port:',i,' | State:', RED+f'{res}'+ENDC)
+        continue
+
+    print('| Port:',i,' | State:', res)
+
+totalTime = time.time() - startTime
+totalTime='%.3f'%totalTime
+print(PURPLE+f"\n[+] Scan Completed\n[+] Time Taken : {totalTime}s\n")
